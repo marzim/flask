efc43b95e8628f47f35a05b862fa5a734afe13ab
marzim <marvin.casagnap@gmail.com>
2015-08-23 13:51:33 +0800

    commit changes

--- views.py
+++ views.py
@@ -5,5 +5,15 @@ from app import app
 @app.route('/index')
 
 def index():
+    posts = [
+        {
+            'author': {'nickname' : 'marvin'},
+            'body' : 'The three black wizard!'
+        },
+        {
+            'author': {'nickname' : 'matt'},
+            'body' : 'The quick brown fox jump over the lazy dog!'
+        }
+    ]
     user = {'nickname':'carlos'}
-    return render_template('index.html',title='Home',user=user)
+    return render_template('index.html',title='Home',user=user,posts=posts)
