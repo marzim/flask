89ae91949dfd41ab08047397940e126d055647f8
marzim <marvin.casagnap@gmail.com>
2015-08-23 14:33:06 +0800

    commit changes

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -17,3 +18,11 @@ def index():
     ]
     user = {'nickname':'carlos'}
     return render_template('index.html',title='Home',user=user,posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenId="%s", remember_me=%s' % (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
